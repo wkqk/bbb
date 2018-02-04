dfa42b333d85c8b66e37a5f6b73a32b2eb3676e8
wkqk <brew_m@naver.com>
2018-02-04 18:23:05 +0900

    java sum file add

---
+++ src/Sum.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		//?…? ¥ : ?‚¤ë³´ë“œë¡? ?‘?ˆ˜?˜ ?…? ¥?„ ë°›ëŠ”?‹¤
+		//ì¶œë ¥: ?™”ë©´ì— ?‘?ˆ˜?˜ ?•©?„ ì¶œë ¥?•œ?‹¤.
+		
+		int num1,num2;
+		System.out.println("?‘?ˆ˜ë¥? ?…? ¥?•˜?„¸?š”.");
+		Scanner scan=new Scanner(System.in);
+		num1=scan.nextInt();
+		num2=scan.nextInt();
+		System.out.println("?‘?ˆ˜?˜ ?•©?? = "+(num1+num2));
+
+	}
+
+}
